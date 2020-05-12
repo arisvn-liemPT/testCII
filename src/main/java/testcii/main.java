3f37f54a3471c5f29ee435d3cf142a13c59208a5
Liem <pliem1991@gmail.com>
2020-05-12 12:48:28 +0700

    Update sss

--- src/main/java/testcii/main.java
+++ src/main/java/testcii/main.java
@@ -3,7 +3,7 @@ package testcii;
 public class main {
     
     public static void main(String[] args) {
-        System.out.println("CCCCCCCCCCCCCCCC");
+        System.out.println("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
     }
 
 }
